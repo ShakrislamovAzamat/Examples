bec0e7625f7e76caa23b645da45268463860c2f4
Azamat <sch_azamat@mail.ru>
2023-04-22 22:19:33 +0500

    get ready

---
+++ example008_friends/Program.cs
@@ -0,0 +1,19 @@
+﻿int count = 0, distance = 10000, time = 0,
+    fFSpeed = 1, sFSpeed = 2,
+    dogSpeed = 5, friend = 2;
+while (distance > 10)
+{
+    if (friend == 1)
+        {
+        friend = 2;
+        time = distance / (fFSpeed + dogSpeed);
+        }
+    else
+        {
+        friend = 1;
+        time = distance / (sFSpeed + dogSpeed);
+        }
+    distance = distance - (fFSpeed + sFSpeed) * time;
+    count++;
+}
+Console.WriteLine($"Собака пробежит {count} раз");
